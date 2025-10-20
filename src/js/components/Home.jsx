34fc8f3ba9ea1f914507f007e066d869dc274f15
Claudia Adrio <c.adrio@outlook.com>
2025-10-20 19:16:25 +0000

    ©

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	
+	const [numero, setNumero] = useState(0)
+	const aumentarNumero = () => {
+		setNumero(numero+1)
+	
+	}
+
+
+
+
+setInterval(function () {element.innerHTML += "Hello"}, 1000);
+
+
+
+
+
+
+	const interval = setInterval(aumentarNumero, 500)
+	setInterval(function (){aumentarNumero += })
 	return (
 		<div className="text-center">
-            
+           {numero}
+			<button onClick={aumentarNumero}>Cambiar número</button>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
 		</div>
 	);
 };
+
+
 
 export default Home;
