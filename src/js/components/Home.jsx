dd4785ec9a8841dfb817ea54bff6693973e076ec
Eduard Quiroga <edykiro@gmail.com>
2025-10-26 18:30:19 +0100

    Cambio de codigo de Home a SecondsCounter

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,33 +1,16 @@
 import SecondsCounter from "./SecondsCounter"
-import { useState, useEffect } from "react"
-
 
 const Home = () => {
-	const [seconds, setSeconds] = useState(0)
 
-	let intervalId = null
-	useEffect(() => {
-		intervalId = setInterval(() => {
-			setSeconds(prev => prev + 1)
-		}, 1000);
-
-		// Limpiar el intervalo cuando el componente se desmonte
-		return () => clearInterval(intervalId);
-	}, []);
-
-	const reiniciarSegundero =() => {
-		setSeconds(0)
-	}
 	return (
+		<>
+		<SecondsCounter/>
+		</>
 
-		<div className="d-flex justify-content-center">
-			{/* <h1>La variable second tiene el valor: {seconds}</h1>*/}
-			<SecondsCounter counter={seconds}/>
-			<button className="btn btn-outline-secondary col-1 rounded-5" onClick={reiniciarSegundero}>Reiniciar<br/>segundero
-			</button>
-		</div>
 	)
 }
 export default Home;
+
+
 
 
--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,14 +1,37 @@
 import "../../styles/SecondsCounter.css"
+import { useState , useEffect } from "react"
 
 
-const SecondsCounter = (props) => {
+const SecondsCounter = () => {
+    	const [seconds, setSeconds] = useState(0)
+
+	let intervalId = null
+	useEffect(() => {
+		intervalId = setInterval(() => {
+			setSeconds(prev => prev + 1)
+		}, 1000);
+
+		// Limpiar el intervalo cuando el componente se desmonte
+		return () => clearInterval(intervalId);
+	}, []);
+
+	const reiniciarSegundero =() => {
+		setSeconds(0)
+	}
     return (
-
-                <div className="d-flex col-5 seconds-counter @font-face rounded-3">
-                            <div className="col-6 fa-regular fa-clock"></div>
-                            
-                            <div className="col-6">{props.counter}</div>
+        <>
+        <div className="d-flex flex-column align-items-center">
+            <div className="d-flex seconds-counter">
+                <div className="me-2">
+                    MALA
                 </div>
+                <div className="">
+                    {seconds}
+                </div>
+            </div>
+            <button className="mt-3" onClick={reiniciarSegundero}>Hola</button>
+        </div>
+        </>      
     )
 
 }
